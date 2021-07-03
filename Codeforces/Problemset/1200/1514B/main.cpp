96dd5763ce0add4fb9d88c9c7d10576873a17e8e
Jatin Dholakia <jdholakia@enphaseenergy.com>
2021-07-03 11:20:35 +0530

    1514B

---
+++ Codeforces/Problemset/1200/1514B/main.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define For(i,x,n) for(int i=x;i<n;i++)
+#define pb push_back
+#define print(vec) For(i,0,vec.size()) {cout<<vec[i]<<" ";} cout<<'\n';
+#define pii pair<int,int>
+#define F first
+#define S second
+#define set(x) cout.precision(x);cout << fixed;
+const int mod = 1'000'000'007;
+const int INF = 2'000'000'000;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    ll ans = 1;
+    For(i, 0, k){
+        ans = ((ans % mod) * (n % mod)) % mod;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        // freopen("output.txt","w",stdout);
+    #endif
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+    return 0;
+}
